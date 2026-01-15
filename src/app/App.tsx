2b91e0f630873010837d51f91b560d5454064812
Sakshidd31 <sakshi31.hrishabraj@gmail.com>
2026-01-15 11:08:42 +0530

    ROUTING

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -87,6 +87,7 @@ import Employee from '@/features/projectMaster/pages/Employee';
 import EmployeeDocument from '@/features/employeeMaster/pages/EmployeeDocument';
 import Rent from '@/features/rent/pages/Rent';
 import Event from '@/features/event/pages/Event';
+import AttendanceCalendar from '@/features/attendanceCalendar/pages/AttendanceCalendar';
 import CompOff from '@/features/compOff/pages/compoff';
 import LeaveCreditDebit from '@/features/leaveCreditDebit/pages/LeaveCreditDebit';
 import AddUpdateLeaveCreditDebit from '@/features/leaveCreditDebit/pages/AddUpdateLeaveCreditDebit';
@@ -243,6 +244,7 @@ function App() {
 
 
             <Route path="event" element={<Event />} />
+            <Route path="attendanceCalendar" element={<AttendanceCalendar />} />
 
             {/* SALES */}
             <Route path="channelPartner" element={<ChannelPartner />} />

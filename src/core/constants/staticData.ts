e1af21e8ef31f0cbc1feb4caafd76f5124df6481
Prachin1206 <prachinbari91@gmail.com>
2026-02-12 12:31:25 +0530

    SALE

--- src/core/constants/staticData.ts
+++ src/core/constants/staticData.ts
@@ -54,7 +54,7 @@ export const MASTER_DATA = {
 
   flat_unit_Type: ['Commercial', 'Gym', 'Residential', 'Void'],
 
-  residential_flat_configuration_Type: ['1 RK', '1 BHK', '2 BHK', '3 BHK', '4 BHK', '1 + 1 JODI','2 + 1 JODI','2 + 2 JODI','2 + 3 JODI','PENTHOUSE'],
+  residential_flat_configuration_Type: ['1 RK', '1 BHK', '2 BHK', '3 BHK', '4 BHK', '1 + 1 JODI', '2 + 1 JODI', '2 + 2 JODI', '2 + 3 JODI', 'PENTHOUSE'],
 
   commercial_flat_configuration_Type: ['OFFICE', 'SHOP'],
 
@@ -70,7 +70,7 @@ export const MASTER_DATA = {
 
   unit_sqft_lumsum: ['Per Sq Ft', 'Lump Sum'],
 
-  business_category: ['Commercial', 'Gym', 'Residential', 'School'],
+  business_category: ['Commercial', 'Mixed Use', 'Residential'],
 
   project_scheme: ['BMC', 'MHADA', 'SRA'],
 
@@ -90,7 +90,7 @@ export const MASTER_DATA = {
 
   occupationType: ['Business', 'Homemaker', 'Professional', 'Salaried', 'Retired',],
 
-  budget: ['<1', '2', '3','4', '5', '6','8','10','12','15','20','25+'],
+  budget: ['<1', '2', '3', '4', '5', '6', '8', '10', '12', '15', '20', '25+'],
 
   accomodation: ["Rented", "Self-Owned",],
 
@@ -100,13 +100,13 @@ export const MASTER_DATA = {
 
   source: ['Channel Partner', 'Direct Walking'],
 
-  subSources: ['Advertisement', 'Exhibition', 'Employee Reference', 'HRR Website', 'Loyalty','Management Reference', 'Property Search Portal','SMS', 'Site Branding','Reference','Other'],
+  subSources: ['Advertisement', 'Exhibition', 'Employee Reference', 'HRR Website', 'Loyalty', 'Management Reference', 'Property Search Portal', 'SMS', 'Site Branding', 'Reference', 'Other'],
 
   subSubsource: ['Facebook', 'Hoarding', 'Instagram', 'Google Ads', 'Newspaper'],
-  
-  subSubsource_channelPartner: ['Channel Partner Data Calling','Channel Partner Walked IN','Digital Activity'],
 
-  finalStage: ['Booking Done', 'Blocked', 'Cancelled', 'Negotiation','Lost', 'Retention', 'Re - Visit Scheduled','Re - Visit Proposed', 'Site Visit','Unit Selection / Blocked'],
+  subSubsource_channelPartner: ['Channel Partner Data Calling', 'Channel Partner Walked IN', 'Digital Activity'],
+
+  finalStage: ['Booking Done', 'Blocked', 'Cancelled', 'Negotiation', 'Lost', 'Retention', 'Re - Visit Scheduled', 'Re - Visit Proposed', 'Site Visit', 'Unit Selection / Blocked'],
 
   finalStageDetail: ['TimeLine Issue', 'Location Issue', 'Low Budget', 'Did Not Like Project', 'Other Issues'],
 
@@ -120,7 +120,7 @@ export const MASTER_DATA = {
 
   sourceOfFunding: ['Loan', 'Self-funded', 'Sale Of Property'],
 
-  ethnicity: ['Bengali', 'Christian', 'Gujarati', 'Jain', 'Muslim', 'Marwari', 'Maharashtrian', 'North Indian', 'Parsi','Punjabi', 'Sindhi', 'South Indian', 'Others'],
+  ethnicity: ['Bengali', 'Christian', 'Gujarati', 'Jain', 'Muslim', 'Marwari', 'Maharashtrian', 'North Indian', 'Parsi', 'Punjabi', 'Sindhi', 'South Indian', 'Others'],
 
   nationality: ['Indian', 'NRI'],
 

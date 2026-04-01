ca8b0ddc64cab64a050aa5f9dac85a364a784b6a
Prachin1206 <prachinbari91@gmail.com>
2026-04-01 14:10:53 +0530

    First Commit

--- src/core/config/environment.ts
+++ src/core/config/environment.ts
@@ -4,10 +4,9 @@ export interface EnvironmentConfig {
   apiTimeout: number;
   environment: "DEVELOPMENT" | "PRODUCTION";
 }
-
 //============================ [ DEVELOPMENT CONFIGURATION ] ========================================================
 const developmentConfig: EnvironmentConfig = {
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || "https://localhost:7034/api",
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || "http://45.126.168.219:402/api",
 
   apiKey: import.meta.env.VITE_API_KEY || "TpBgVhTojiO2Ue3kztqI2N/lv7NgtUZgBs0olZHw6HQ/phXqXpadHfJO059HQVTLNyUDi/AbSPGcmP3MdRNtBw==",
   apiTimeout: 30000,
@@ -24,9 +23,8 @@ const productionConfig: EnvironmentConfig = {
 
 //============================ [GET CURRENT ENVIRONMENT ] ========================================================
 const getCurrentEnvironment = (): "DEVELOPMENT" | "PRODUCTION" => {
-
-  const env = (import.meta.env.VITE_APP_ENVIRONMENT || import.meta.env.MODE)?.toUpperCase();
-
+  //const env = import.meta.env.VITE_APP_ENVIRONMENT || import.meta.env.MODE;
+  const env = "PRODUCTION";
   switch (env) {
     case "PRODUCTION":
       return "PRODUCTION";

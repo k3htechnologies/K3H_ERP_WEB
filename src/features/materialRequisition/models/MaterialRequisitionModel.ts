f95299af5c21b33dae9a107e495176eff9731c80
Sakshidd31 <sakshi31.hrishabraj@gmail.com>
2026-04-07 14:13:17 +0530

    materialRequisition_DataSource

---
+++ src/features/materialRequisition/models/MaterialRequisitionModel.ts
@@ -0,0 +1,95 @@
+import type { ApiResponse } from "@/core/api/ApiResponse"
+
+export interface FilterWithPaginationMaterialRequisition {
+    PageSize: number
+    PageNumber: number
+    ProjectId: number
+    MaterialRequisitionId?: number | 0
+    SystemGeneratedCode?: string | null
+    FromDate?: string | null
+    ToDate?: string | null
+    MaterialRequisitionStage?: string | null
+    MaterialRequisitionStatus?: string | null
+    SortBy? : string
+    ExportType? : "PDF" | "Excel"
+}
+
+export interface MaterialRequisitionData {
+    MaterialRequisitionId:number;
+    Uniquekey:string;
+    SystemGeneratedCode:string;
+    ProjectId:number;
+    ProjectName:string;
+    AttachmentsURL:string;
+    Remarks: string;
+    ClientRegistrationId:number;
+    MaterialRequisitionStage:string;
+    MaterialRequisitionStatus:string;
+    FinalVendor:string;
+    IsSplit:boolean;
+    IsCopy:boolean;
+    IsRequisitionAction:boolean;
+    IsApprovalVendorFinalization:boolean;
+    IsApprovalInvoice:boolean;
+    VendorFinalizationApprovalStatus: string;
+    InvoiceApprovalStatus:string;
+    CreatedById:number;
+    CreatedBy:string;
+    CreatedDate:Date;
+    ModifiedById:number;
+    ModifiedBy:string;
+    ModifiedDate:null;
+    PaidAmount:number;
+    TotalPoAmount:number;
+    TotalInvoiceAmount:number;
+    TotalInvoice:number;
+    PurchaseOrderURL:string;
+    MaterialRequisitionDetailData:MaterialRequisitionDetailData[];
+}
+export interface MaterialRequisitionDetailData{
+    MaterialRequisitionDetailId:number;
+    Uniquekey:string;
+    MaterialMasterId:number;
+    MaterialCode:string;
+    MaterialName:string;
+    SubMaterialName:string;
+    SubMaterialMasterId:number;
+    MaterialQuantity:number;
+    UomMasterId:number;
+    UomCode:string;
+    Uom: string;
+    RequiredDate:Date;
+    MaterialReceivedQuantityTillDate: number;
+    CreatedById:number;
+    CreatedBy:string;
+    CreatedDate:Date;
+    ModifiedById:number;
+    ModifiedBy:string;
+    ModifiedDate:null;
+}
+export interface AddUpdateMaterialRequisitionRequest{
+    MaterialRequisitionId:number;
+    Uniquekey: string;
+    IsCopy:boolean;
+    ProjectId:number;
+    Remarks: string | null;
+    IsSplit: boolean;
+    AttachmentsURL: string | null;
+    RemoveAttachmentsURL: string | null;
+    MaterialRequisitionDetailJSON: string;
+ }
+export interface DeleteMaterialRequisitionRequest {
+    MaterialRequisitionId : number,
+    Uniquekey: string | null,
+    ProjectId: number | 0,
+}
+export interface CloseMaterialRequisitionRequest {
+    MaterialRequisitionId : number,
+    Uniquekey: string | null,
+    ProjectId: number | 0,
+
+ }
+
+export type MaterialRequisitionListResponse = ApiResponse<MaterialRequisitionData[]>;
+export type MaterialRequisitionSaveReponse = ApiResponse<MaterialRequisitionData>;
+export type MaterialRequisitionDeleteResponse = ApiResponse<number>;

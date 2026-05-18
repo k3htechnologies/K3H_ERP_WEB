b07a1b88d6f5bcdba89d0f2b6e5669e20d869afc
Sakshidd31 <sakshi31.hrishabraj@gmail.com>
2026-05-18 18:20:46 +0530

    Revert "Merge branch 'Phase_2_Purchase' of https://github.com/k3htechnologies/K3H_ERP_WEB into Phase2_JyotiYadav"
    This reverts commit d1d8e1c5599f92e33451a64e72d4d249e63d06b7, reversing
    changes made to 749ee984e0126061c5dc459b318f2d85b380391d.

--- src/features/stockManagement/pages/ViewStockManagement.tsx
+++ src/features/stockManagement/pages/ViewStockManagement.tsx
@@ -342,7 +342,6 @@ import { useMenuPermissions } from "@/features/menu/hooks/useMenuPermissions";
 import StocksHistory from "../components/StocksHistory";
 import { MaterialOut } from "../components/Materialout";
 import MaterialIn from "../components/Materialin";
-import StockSummary from "../components/StockSummary";
 // import { StockSummary } from "../components/StockSummary";
 
 export const ViewStockManagement: React.FC = () => {
@@ -359,10 +358,9 @@ export const ViewStockManagement: React.FC = () => {
     const { canExport } = useMenuPermissions();
 
     const MaterialRequisitionTabList = [
-        { id: 'Summary', label: 'Summary' },
         { id: 'History', label: 'History' },
         { id: 'Material In', label: 'Material In' },
-        { id: 'Material Issued', label: 'Material Issued' },
+        { id: 'Material Out', label: 'Material Out' },
     ];
 
     const [activeTab, setActiveTab] = useState<string>(MaterialRequisitionTabList[0].id);
@@ -431,11 +429,10 @@ export const ViewStockManagement: React.FC = () => {
                     onTabChange={(t) => setActiveTab(t.id)}
                 />
             </div>
-
-            {activeTab === 'Summary' && <StockSummary />}
+            {/* {activeTab === 'Summary' && <StockSummary />} */}
             {activeTab === 'History' && <StocksHistory />}
             {activeTab === 'Material In' && <MaterialIn />}
-            {activeTab === 'Material Issued' && <MaterialOut />}
+            {activeTab === 'Material Out' && <MaterialOut />}
 
         </div>
     )
